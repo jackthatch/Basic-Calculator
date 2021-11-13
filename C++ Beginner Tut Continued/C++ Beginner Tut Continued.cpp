a8b3f120d4828532a619e4f78a059471932452a8
jacktthatch <rthat@DESKTOP-RFB8DU4>
2021-11-12 19:53:37 -0600

    removed note along with "using namespace std" and replaced lines with "std:: " as needed.

--- C++ Beginner Tut Continued/C++ Beginner Tut Continued.cpp
+++ C++ Beginner Tut Continued/C++ Beginner Tut Continued.cpp
@@ -1,21 +1,6 @@
 #include <iostream>
-using namespace std;
 
-/*
-bool anotherCalc(double result) {                      //This function displays the answer and asks//
-	int moreCalc;
-	cout << endl << "The answer is: " << result << endl;
-	cout << "Would you like to make another calculation, Yes = 0, No = 1";
-	cin >> moreCalc;
-	if (moreCalc == 0) {
-	
-	}
-	else {
-		cout << "Thank you for playing !";
-	}
-	return true;								//if another calculation will be made//return 1;
-}
-*/
+
 int calc(double numOne, double numTwo, char op) {
 	double result = 0;
 	switch (op) {
@@ -34,37 +19,31 @@ int calc(double numOne, double numTwo, char op) {
 	default:
 		result = 0;
 	}
-	cout << "The answer is: " << result << endl;
+	std:: cout << "The answer is: " << result << std:: endl;
 	return 0;
-
 }
-
 
 int main()
 {
-	string anotherCalculation;
+	std:: string anotherCalculation;
 	double numOne, numTwo;
 	char op;
-	cout << "Please select your first digit" << endl;
-	cin >> numOne;
-	cout << "Please select your operand (+, -, *, or / )" << endl;
-	cin >> op;
-	cout << "Please select your second digit" << endl;
-	cin >> numTwo;
+	std:: cout << "Please select your first digit" << std:: endl;
+	std:: cin >> numOne;
+	std:: cout << "Please select your operand (+, -, *, or / )" << std:: endl;
+	std:: cin >> op;
+	std:: cout << "Please select your second digit" << std:: endl;
+	std:: cin >> numTwo;
 
 	calc(numOne, numTwo, op);
 
-	cout << "Would you like to make another calculation? Types yes or no." << endl;
-	cin >> anotherCalculation;
+	std:: cout << "Would you like to make another calculation? Types yes or no." << std:: endl;
+	std:: cin >> anotherCalculation;
 	if (anotherCalculation == "yes" || anotherCalculation == "Yes") {
 		main();
 	}
 	else {
-		cout << "Thank you for playing !";
+		std:: cout << "Thank you for playing !";
 	}
 	return 0;
 }
-
-
-
-
